c152ee5aaf5bf8e7f00f18e8008261e9d74aa01a
Denil Dominic <ddominic@depaul.edu>
2025-05-06 22:46:20 -0500

    Page 2 edits

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,19 +24,19 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a CS student at DePaul University in Chicago.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Java, Python, JavaScript, HTML, CSS, C, C++",
+  "Web Development",
+  "Backend Development",
+  "Software Testing",
+  "Android Development",
+  "Project management experience",
 ];
 
 /**
