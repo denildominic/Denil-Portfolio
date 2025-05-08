0d0fd1b8e01eb5e22f8ae6cb534d1683070ed7a2
Denil Dominic <ddominic@depaul.edu>
2025-05-08 14:13:22 -0500

    Update Portfolio.jsx

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -39,20 +39,20 @@ const projectList = [
   {
     title: "Project 2",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "Coming soon",
+    url: "",
   },
   {
     title: "Project 3",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+      "Coming soon",
+    url: "",
   },
   {
     title: "Project 4",
     description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "Coming soon",
+    url:"" ,
   },
 ];
 
